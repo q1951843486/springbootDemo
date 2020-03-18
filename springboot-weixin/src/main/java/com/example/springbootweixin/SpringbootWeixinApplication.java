a5a22e78072cf1598a7280da99142aaefbae6325
liushiwei <liushiwei>
2020-03-18 16:59:31 +0800

    生成小程序二维码

---
+++ springboot-weixin/src/main/java/com/example/springbootweixin/SpringbootWeixinApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootweixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeixinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWeixinApplication.class, args);
+    }
+
+}
---
+++ springboot-weixin/src/test/java/com/example/springbootweixin/SpringbootWeixinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootweixin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeixinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
