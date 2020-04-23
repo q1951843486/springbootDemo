e899f1cbf3545dd7c244373314e558f5a0e2d499
liushiwei <liushiwei>
2020-04-23 15:44:55 +0800

    arangodb

---
+++ springboot-arangdb/src/main/java/com/example/springbootarangdb/SpringbootArangdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootarangdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootArangdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootArangdbApplication.class, args);
+    }
+
+}
---
+++ springboot-arangdb/src/main/java/com/example/springbootarangdb/controller/ArangodbController.java
@@ -0,0 +1,16 @@
+package com.example.springbootarangdb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @ClassName arangodbController
+ * @Author Administrator
+ * @date 2020.04.21 09:48
+ */
+@RestController
+public class ArangodbController {
+
+
+
+}
