98da6d91777b74b4f86d1b05259a651a7d718732
liushiwei <liushiwei>
2020-04-30 17:44:01 +0800

    mongodb

--- springboot-mongodb/src/main/java/com/meiya/config/MongodbConfig.java
+++ springboot-mongodb/src/main/java/com/meiya/config/MongodbConfig.java
@@ -20,22 +20,19 @@ import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
  */
 @Configuration
 public class MongodbConfig {
-    @Configuration
-    public class MongoConfig {
-        @Bean
-        public MappingMongoConverter mappingMongoConverter(MongoDbFactory factory, MongoMappingContext context, BeanFactory beanFactory) {
-            DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
-            MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, context);
-            try {
-                mappingConverter.setCustomConversions(beanFactory.getBean(CustomConversions.class));
-            } catch (NoSuchBeanDefinitionException ignore) {
-            }
-
-            // Don't save _class to mongo
-            mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
-
-            return mappingConverter;
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDbFactory factory, MongoMappingContext context, BeanFactory beanFactory) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter mappingConverter = new MappingMongoConverter(dbRefResolver, context);
+        try {
+            mappingConverter.setCustomConversions(beanFactory.getBean(CustomConversions.class));
+        } catch (NoSuchBeanDefinitionException ignore) {
         }
+
+        // Don't save _class to mongo
+        mappingConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return mappingConverter;
     }
 
 
